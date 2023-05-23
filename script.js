a1b994d3b12563f14139ffbc341b1984583a1dc2
NCW1 <ngenchangwang@gmail.com>
2023-05-23 11:48:43 +0000

    Allow users to submit form data to Google Sheet

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ window.addEventListener("scroll", () => {
 backToTopBtn.addEventListener("click", () => {
 	window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// Submit to Google Sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzViTYVOMxpwOg_9dsPiOHR6EcoCtwOl_uoowyIHHNtLPyDkCb9rLX1COHn-d6STki7/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+		.then(response => {
+			msg.innerHTML = "Message Sent Successfully!"
+			setTimeout(function() {
+				msg.innerHTML = ""
+			}, 5000)
+			form.reset()
+		})
+		.catch(error => console.error('Error!', error.message))
+})
